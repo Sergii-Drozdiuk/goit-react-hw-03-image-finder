ae243c3280251097c71345133ef1af792273721d
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-10-17 01:18:50 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,15 +3,13 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 
 export const ImageGallery = ({ images }) => {
   return (
-    <div className='flex flex-wrap'>
-      <ul className='mx-auto grid grid-cols-1 gap-4 pl-1 pr-4 sm:grid-cols-2 md:grid-cols-3'>
-        {images.map(({ id, webformatURL, largeImageURL, tags }) => (
-          <li key={id} className='rounded-md shadow-md hover:shadow-lg'>
-            <ImageGalleryItem src={webformatURL} tags={tags} img={largeImageURL} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className='mx-auto grid grid-cols-1 gap-4 pl-1 pr-4 sm:grid-cols-2 md:grid-cols-3'>
+      {images.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <li key={id} className='rounded-md shadow-md hover:shadow-lg'>
+          <ImageGalleryItem src={webformatURL} tags={tags} img={largeImageURL} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
