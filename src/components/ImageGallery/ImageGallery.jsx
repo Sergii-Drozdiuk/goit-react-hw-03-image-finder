7b10071a96c49f056356a8c53f779b4d9780835a
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-10-17 01:25:12 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,7 +3,7 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 
 export const ImageGallery = ({ images }) => {
   return (
-    <ul className='mx-auto grid grid-cols-1 gap-4 pl-1 pr-4 sm:grid-cols-2 md:grid-cols-3'>
+    <ul className='mx-auto grid grid-cols-1 gap-4 pl-1 pr-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
       {images.map(({ id, webformatURL, largeImageURL, tags }) => (
         <li key={id} className='rounded-md shadow-md hover:shadow-lg'>
           <ImageGalleryItem src={webformatURL} tags={tags} img={largeImageURL} />
